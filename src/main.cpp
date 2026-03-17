b3b24e3852be0c53e799e921e85354bf12e5af4e
Cardaver <michal.sadecki@proton.me>
2026-03-17 17:58:48 +0100

    fsdf

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
-#include "esp32-hal-gpio.h"
 #include <Arduino.h>
-
+#include "../lib/Manager/src/DiodeManager.hh"
+#include "../lib/Manager/src/SensorManager.hh"
 const uint8_t LED = 2;
+DiodeManager diode;
+SensorManager sensor;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("ESP32 gotowe!");
-    pinMode(LED, OUTPUT);
+    diode.setup();
+
+    while(!Serial) {
+        delay(10);
+    }
+
+    delay(1000);
 }
 
 void loop() {
-    digitalWrite(LED, HIGH);
+    Serial.println(sensor.measure().c_str());
+    diode.setOn();
     delay(500);
-    digitalWrite(LED, LOW);
+    diode.setOff();
     delay(500);
 }
