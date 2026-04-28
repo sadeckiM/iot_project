8dfbaacbe53a248772344b27afdd163595f35067
Arkadiusz Lechwar <ar.lechwar@gmail.com>
2026-04-28 18:35:12 +0200

    cos

--- backend6/src/app.js
+++ backend6/src/app.js
@@ -1,10 +1,13 @@
 import cors from "cors";
 import express from "express";
+import iotRoutes from "./controller/iot.rest.js"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/iot", iotRoutes)
 
 app.get("/api/v1/health", async (req, res) => {
   res.status(200).json({ status: "ok" })
---
+++ backend6/src/controller/iot.rest.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { addMeasurementData, getMeasurementData } from "../database/mock.connector";
+
+const router = Router();
+
+router.get("/data", async (req, res) => {
+    // console.log({ ...req.query }),
+    // console.log({ ...req.body })
+    const data = await getMeasurementData();
+
+    res.status(200).json({ status: "ok", data: data});
+});
+
+router.post("/data", async (req, res) => {
+    // console.log({ ...req.query }),
+    // console.log({ ...req.body })
+
+    const data = await getMeasurementData();
+
+    res.status(200).json({ status: "ok"})
+});
+
+export default router;
